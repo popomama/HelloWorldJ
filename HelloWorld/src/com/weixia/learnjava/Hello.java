79acb49816c7cc26acdde5cb1dc3f168386de076
WeiXia <wei_xia@hotmail.com>
2017-07-25 15:24:13 -0400

    added Great!

---
+++ HelloWorld/src/com/weixia/learnjava/Hello.java
@@ -0,0 +1,11 @@
+package com.weixia.learnjava;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Great!");
+
+	}
+
+}
